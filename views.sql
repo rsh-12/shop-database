23219fc7f843997744daf1ec9e5986ed541943d8
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-02-13 14:36:46 +0500

    Add sellers view

--- views.sql
+++ views.sql
@@ -10,4 +10,24 @@ where products.id not in (select product_id
                           where datetime < current_date
                             and datetime >= current_date - interval '1' month);
 
-select * from last_month_products;
+select *
+from last_month_products;
+
+
+-- №4
+create view last_month_sellers as
+select name as shop_name, first_name, last_name
+from employees e
+         join shops s on e.shop_id = s.id
+where job_name ilike 'продавец'
+  and e.id not in
+      (select seller_id
+       from purchases
+       where datetime < current_date
+         and datetime >= current_date - interval '1' month);
+
+select *
+from last_month_sellers;
+
+
+-- №5

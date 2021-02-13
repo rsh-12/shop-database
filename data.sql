a77361a64b35e6ef2b63b1b13b27ff4367aa42e3
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-02-13 14:36:56 +0500

    Rename job names

--- data.sql
+++ data.sql
@@ -31,41 +31,41 @@ VALUES ('Shop Name 1', 'Sverdlovskaya obl.', 'Yekaterinburg', 'Address 32'),
 
 -- employees
 insert into employees(first_name, last_name, phone, e_mail, job_name, shop_id)
-VALUES ('Полина', 'Большакова', '8-922-231-000', 'aojv@mail.ru', 'seller', 1),
-       ('Александр', 'Бычков', '8-922-231-001', 'kggfpxw@yandex.ru', 'seller', 2),
-       ('Ярослава', 'Волкова', '+7-922-231-002', 'yaroslava@yandex.ru', 'seller', 3),
-       ('Таисия', 'Демидова', '8-922-231-003', 'tasya@gmail.com', 'seller', 4),
-       ('Олег', 'Золотарев', '8-922-231-004', 'oleggg@mail.ru', 'manager', 5),
-       ('Даниил', 'Иванов', '8-922-231-005', 'ahbg@yandex.ru', 'seller', 6),
-       ('Михаил', 'Измайлов', '8-922-231-006', 'misha@yandex.ru', 'seller', 7),
-       ('Екатерина', 'Калашникова', '8-922-231-007', 'qwerty@mail.ru', 'seller', 8),
-       ('Максим', 'Кондратьев', '8-922-231-008', 'dsfjakl@mail.ru', 'seller', 9),
-       ('Вадим', 'Логинов', '8-922-231-009', 'fdjsaikfh@mail.ru', 'manager', 10),
-       ('Анна', 'Миронова', '8-922-231-010', 'anna@mail.ru', 'seller', 11),
-       ('Милана', 'Михайлова', '8-922-231-011', 'pillow@mail.ru', 'seller', 12),
-       ('Анастасия', 'Михайлова', '8-922-231-012', 'olla@mail.ru', 'manager', 1),
-       ('Михаил', 'Некрасов', '8-922-231-013', 'daily@mail.ru', 'seller', 2),
-       ('Мирослава', 'Никитина', '8-922-231-014', 'lila@mail.ru', 'seller', 3),
-       ('Ксения', 'Овчинникова', '8-922-231-015', 'sadpod@mail.ru', 'seller', 4),
-       ('Артур', 'Павлов', '8-922-231-016', 'artur@mail.ru', 'seller', 5),
-       ('Кирилл', 'Попов', '8-922-231-017', 'plsql@mail.ru', 'seller', 6),
-       ('Татьяна', 'Савельева', '8-922-231-018', 'sql@mail.ru', 'manager', 7),
-       ('Сергей', 'Соловьев', '8-922-231-019', 'jinja@mail.ru', 'seller', 8),
-       ('Ольга', 'Соловьева', '8-922-231-020', 'olga@gmail.com', 'seller', 9),
-       ('Елизавета', 'Сорокина', '8-922-231-021', 'liza@gmail.com', 'seller', 10),
-       ('Руслан', 'Трифонов', '8-922-231-022', 'ruslan@gmail.com', 'seller', 11),
-       ('Михаил', 'Успенский', '8-922-231-023', 'uspex@gmail.com', 'manager', 12);
+VALUES ('Полина', 'Большакова', '8-922-231-000', 'aojv@mail.ru', 'продавец', 1),
+       ('Александр', 'Бычков', '8-922-231-001', 'kggfpxw@yandex.ru', 'продавец', 2),
+       ('Ярослава', 'Волкова', '+7-922-231-002', 'yaroslava@yandex.ru', 'продавец', 3),
+       ('Таисия', 'Демидова', '8-922-231-003', 'tasya@gmail.com', 'продавец', 4),
+       ('Олег', 'Золотарев', '8-922-231-004', 'oleggg@mail.ru', 'менеджер', 5),
+       ('Даниил', 'Иванов', '8-922-231-005', 'ahbg@yandex.ru', 'продавец', 6),
+       ('Михаил', 'Измайлов', '8-922-231-006', 'misha@yandex.ru', 'продавец', 7),
+       ('Екатерина', 'Калашникова', '8-922-231-007', 'qwerty@mail.ru', 'продавец', 8),
+       ('Максим', 'Кондратьев', '8-922-231-008', 'dsfjakl@mail.ru', 'продавец', 9),
+       ('Вадим', 'Логинов', '8-922-231-009', 'fdjsaikfh@mail.ru', 'менеджер', 10),
+       ('Анна', 'Миронова', '8-922-231-010', 'anna@mail.ru', 'продавец', 11),
+       ('Милана', 'Михайлова', '8-922-231-011', 'pillow@mail.ru', 'продавец', 12),
+       ('Анастасия', 'Михайлова', '8-922-231-012', 'olla@mail.ru', 'менеджер', 1),
+       ('Михаил', 'Некрасов', '8-922-231-013', 'daily@mail.ru', 'продавец', 2),
+       ('Мирослава', 'Никитина', '8-922-231-014', 'lila@mail.ru', 'продавец', 3),
+       ('Ксения', 'Овчинникова', '8-922-231-015', 'sadpod@mail.ru', 'продавец', 4),
+       ('Артур', 'Павлов', '8-922-231-016', 'artur@mail.ru', 'продавец', 5),
+       ('Кирилл', 'Попов', '8-922-231-017', 'plsql@mail.ru', 'продавец', 6),
+       ('Татьяна', 'Савельева', '8-922-231-018', 'sql@mail.ru', 'менеджер', 7),
+       ('Сергей', 'Соловьев', '8-922-231-019', 'jinja@mail.ru', 'продавец', 8),
+       ('Ольга', 'Соловьева', '8-922-231-020', 'olga@gmail.com', 'продавец', 9),
+       ('Елизавета', 'Сорокина', '8-922-231-021', 'liza@gmail.com', 'продавец', 10),
+       ('Руслан', 'Трифонов', '8-922-231-022', 'ruslan@gmail.com', 'продавец', 11),
+       ('Михаил', 'Успенский', '8-922-231-023', 'uspex@gmail.com', 'менеджер', 12);
 
 
--- shops-employess: set manager id
+-- shops-employess: set менеджер id
 update shops
-set manager_id = e.id
-from (select * from employees where job_name = 'manager') e
+set менеджер_id = e.id
+from (select * from employees where job_name = 'менеджер') e
 where shops.id = e.shop_id;
 
 
 -- purchases
-insert into purchases(datetime, amount, seller_id)
+insert into purchases(datetime, amount, продавец_id)
 VALUES (current_timestamp - interval '1' month, 18000, 1),
        (current_timestamp - interval '2' month, 6800, 2),
        (current_timestamp - interval '1' month, 3200, 3),
